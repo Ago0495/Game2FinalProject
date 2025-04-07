9e9f0f8bf52943ee65d9ab135406948c2f62c25e
Tspangler1234 <tspangler1234@gmail.com>
2025-04-07 19:04:58 -0400

    Created base scripts for defense and attack

---
+++ Assets/Prefabs/Collectables/DefensePowerUp.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+public class DefensePowerUp : Collectable
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public override void OnCollected()
+    {
+        if (IsServer)
+        {
+            Debug.Log("Defense Power Up collected");
+        }
+
+    }
+}
