8d0be592b4d5bd15ea2a27a918673c1171f922de
EthanDiemer <119145667+EthanDiemer@users.noreply.github.com>
2025-04-11 12:20:06 -0400

    Finished enemyCannon

--- Assets/Prefabs/EnemyCannon/EnemyCannon.cs
+++ Assets/Prefabs/EnemyCannon/EnemyCannon.cs
@@ -6,43 +6,71 @@ public class EnemyCannon : NetworkComponent
 {
     public GameObject target;
     public float detectionRange = 20f;
-    public float fireCooldown = 2f;
-    public Transform firePoint;
-
-    public float lastFireTime = 0f;
+    public int cannonBallSpeed;
+    public float gravity;
 
     public float reloadTime;
     public bool reloading;
+    public Vector3 aimOffset;
 
-    public GameObject cannonballPrefab;
     public Rigidbody MyRig;
 
-    //protected int cannonballPrefab;
+    public int cannonballPrefab;
+
+    public Vector3 lastPosition;
+    public Vector3 lastRotation;
+
+    public float Threashhold;
+    public float Ethreashhold;
+
+
 
     public override IEnumerator SlowUpdate()
     {
-        while (IsServer)
+        while (true)
         {
-            //GameObject[] players = GameObject.FindGameObjectsWithTag("SHIP");
-            //look at player 
-            float dist = Vector3.Distance(transform.position, target.transform.position);
-            if (dist <= detectionRange && !reloading)
+            if (IsServer)
             {
-                FireAtPlayer(target.transform);
-                lastFireTime = Time.time;
+                float distance = (this.transform.position - lastPosition).magnitude;
+                if (distance > Threashhold)
+                {
+                    SendUpdate("POS", this.transform.position.ToString());
+                    lastPosition = this.transform.position;
+                }
+                if ((this.transform.rotation.eulerAngles - lastRotation).magnitude > Threashhold)
+                {
+                    lastRotation = this.transform.rotation.eulerAngles;
+                    SendUpdate("ROT", lastRotation.ToString());
+                }
+
+                if (IsDirty)
+                {
+                    SendUpdate("POS", lastPosition.ToString());
+                    SendUpdate("ROT", lastRotation.ToString());
+                    //animation
+
+                    IsDirty = false;
+                }
             }
-            yield return new WaitForSeconds(0.5f);
+            yield return new WaitForSeconds(MyCore.MasterTimer);
         }
     }
 
     public override void HandleMessage(string flag, string value)
     {
-        
+        if (flag == "POS" && IsClient)
+        {
+            lastPosition = NetworkCore.Vector3FromString(value);
+        }
+        if (flag == "ROT" && IsClient)
+        {
+            lastRotation = NetworkCore.Vector3FromString(value);
+        }
     }
 
     public override void NetworkedStart()
     {
-        
+        target = GameObject.FindGameObjectWithTag("SHIP");
     }
 
     public IEnumerator reload()
@@ -54,34 +82,74 @@ public class EnemyCannon : NetworkComponent
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        target = GameObject.FindGameObjectWithTag("SHIP");
         MyRig = this.GetComponent<Rigidbody>();
+        target = GameObject.FindGameObjectWithTag("SHIP");
     }
 
     // Update is called once per frame
     void Update()
     {
-        //RotateTowards(transform.position - target.transform.position);
-        Vector3 direction = (transform.position - target.transform.position).normalized;
-        Quaternion targetRotation = Quaternion.LookRotation(-direction);
-        transform.rotation = Quaternion.RotateTowards(
-            transform.rotation,
-            targetRotation,
-            20 * Time.deltaTime
-        );
-        if (!reloading)
+        if (IsServer)
         {
-            GameObject tmp = (GameObject)Instantiate(cannonballPrefab, transform.position + transform.forward * 5, Quaternion.identity);
-            reloading = true;
-            StartCoroutine(reload());
+            Vector3 toTarget = target.transform.position - transform.position + aimOffset;
+
+            Vector3 toTargetXZ = new Vector3(toTarget.x, 0, toTarget.z);
+
+            float grav = Mathf.Abs(Physics.gravity.y);
+            float v2 = cannonBallSpeed * cannonBallSpeed;
+            float v4 = v2 * v2;
+            float gx = grav * toTargetXZ.magnitude;
+
+            float discriminant = v4 - grav * (grav * toTargetXZ.magnitude * toTargetXZ.magnitude + 2 * toTarget.y * v2);
+
+            Quaternion targetRotation = Quaternion.identity;
+
+            float sqrtDisc = Mathf.Sqrt(discriminant);
+            float angle = Mathf.Atan((v2 - sqrtDisc) / gx);
+
+            Vector3 flatDir = toTargetXZ.normalized;
+            Quaternion yawRotation = Quaternion.LookRotation(flatDir);
+            if(Mathf.Abs(angle) > 0)
+            {
+                targetRotation = yawRotation * Quaternion.Euler(-Mathf.Rad2Deg * angle, 0, 0);
+
+                transform.rotation = Quaternion.RotateTowards(
+                transform.rotation,
+                targetRotation,
+                20 * Time.deltaTime);
+
+                if (!reloading)
+                {
+                    FireAtPlayer();
+                    StartCoroutine(reload());
+                }
+            }
         }
+        if (IsClient)
+        {
+            float distance = (this.transform.position - this.lastPosition).magnitude;
+            if (distance > Ethreashhold)
+            {
+                this.transform.position = this.lastPosition;
+            }
+            else
+            {
+                this.transform.position = Vector3.Lerp(this.transform.position, lastPosition, Time.deltaTime * 20);
+            }
+
+            this.transform.rotation = Quaternion.Euler(lastRotation);
+        }
+
     }
 
-    void FireAtPlayer(Transform target)
+    void FireAtPlayer()
     {
-        Vector3 direction = (target.position - firePoint.position).normalized;
-        //GameObject projectile = MyCore.NetCreateObject(cannonballPrefab, -1, firePoint.position, Quaternion.LookRotation(direction));
-        //Debug.Log("Cannon fired at: " + target.name);
+        GameObject tempBall = MyCore.NetCreateObject(cannonballPrefab, -1, transform.position + transform.forward * 5, Quaternion.identity);
+        Rigidbody tempRB = tempBall.GetComponent<Rigidbody>();
+        if (tempRB != null)
+        {
+            tempRB.linearVelocity = transform.forward * cannonBallSpeed;
+        }
         reloading = true;
     }
 }
